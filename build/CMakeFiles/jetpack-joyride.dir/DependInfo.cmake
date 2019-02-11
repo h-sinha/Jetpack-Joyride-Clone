7b9221e79b0b59943f0309af04a14176dff375ab
Harsh <harsh.26020@gmail.com>
2019-02-11 22:47:03 +0530

    Added screenshot

---
+++ build/CMakeFiles/jetpack-joyride.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/ball.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/ball.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/bonuscoin.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/bonuscoin.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/bricks.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/bricks.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/coin.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/coin.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/color.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/color.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/dragon.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/dragon.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/enemy1_firelines.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/enemy1_firelines.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/enemy2_firebeam.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/enemy2_firebeam.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/enemy3.boomerang.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/enemy3.boomerang.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/functions.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/functions.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/heart.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/heart.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/ice.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/ice.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/input.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/input.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/magnet.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/magnet.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/main.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/main.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/nonedit.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/nonedit.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/other_handlers.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/other_handlers.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/player.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/player.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/score.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/score.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/semicircle.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/semicircle.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/shield.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/shield.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/speedup.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/speedup.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/sword.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/sword.cpp.o"
+  "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/src/timer.cpp" "/home/harsh/acads/sem4/Graphics/assignments/Jetpack-Joyride-Clone/build/CMakeFiles/jetpack-joyride.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/jetpack-joyride.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jetpack-joyride.dir/src/ball.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/bonuscoin.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/bricks.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/coin.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/color.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/dragon.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/enemy1_firelines.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/enemy2_firebeam.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/enemy3.boomerang.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/functions.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/heart.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/ice.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/input.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/magnet.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/main.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/nonedit.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/other_handlers.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/player.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/score.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/semicircle.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/shield.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/speedup.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/sword.cpp.o"
+  "CMakeFiles/jetpack-joyride.dir/src/timer.cpp.o"
+  "jetpack-joyride.pdb"
+  "jetpack-joyride"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jetpack-joyride.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
